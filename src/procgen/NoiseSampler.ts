4d10367d843de556653b8bd54fb31c9cfef163d2
etienne <em85@pm.me>
2024-03-20 19:19:07 +0000

    feat: support noise parametrisation

--- src/procgen/NoiseSampler.ts
+++ src/procgen/NoiseSampler.ts
@@ -1,11 +1,10 @@
-import { SimplexNoise } from 'three/examples/jsm/math/SimplexNoise.js'
+import { createNoise2D } from 'simplex-noise';
 import { Vector2, Vector3 } from 'three'
-
 import { sanitiseNoise } from '../common/utils'
 
 export type InputType = Vector2 | Vector3
 export type Generator = (input: InputType) => number
-const
+
 export interface Sampler<InputType> {
   parent: any
   // userScale: number;   // scale applied to sampler user input when querying sample
@@ -16,64 +15,70 @@ export interface Sampler<InputType> {
   set config(config: any)
 }
 
-export class CustomSampler<InputType> implements Sampler<InputType> {
-  density: number
-  generator: any
-  constructor(generator: Generator) {
-    this.generator = generator
-    this.density = 1
-  }
-  parent: any
-  eval(input: InputType): number {
-    return this.generator(input)
-  }
-
-  get config() {
-    return {
-      density: this.density
-    }
-  }
-
-  set config(config) {
-    this.density = config.density
-    this.parent?.onChange(this)
-  }
-
+type Harmonic = {
+  period: number,
+  amplitude: number
 }
 
-/**
- * Sampling points from noise source
- */
-export class ProceduralNoiseSampler implements Sampler<InputType> {
-  density: number = 1
-  noiseSource
-  parent: any
-
-  constructor(density = 1, noiseSource = new SimplexNoise()) {
-    this.density = density
-    this.noiseSource = noiseSource
+export class SimplexNoiseSampler implements Sampler<InputType> {
+  density: number;
+  harmonics: Harmonic[] = [];
+  harmonicsAmplitudeSum: number
+  noiseSource: any;
+  noiseParams = {
+    harmonics: {
+      period: 64,
+      count: 1,
+      spread: 2,
+      gain: 1
+    },
   }
+  stats = {
 
-  get config() {
+  }
+  parent: any;
+
+  constructor() {
+    this.noiseSource = createNoise2D()
+    this.density = 128
+  }
+  get config(): any {
     return {
-      density: this.density
+      density: this.density,
+      noiseParams: this.noiseParams
     }
   }
-
-  set config(config) {
-    const density = !isNaN(config.scattering) ? 1 / Math.pow(2, config.scattering) : config.density
+  set config(params: any) {
+    this.noiseParams = params.noise || this.noiseParams
+    const density = !isNaN(params.scattering) ? 1 / Math.pow(2, params.scattering) : params.density
     this.density = !isNaN(density) ? density : this.density
-    this.parent?.onChange(this)
+    this.onChange(this)
+  }
+  onChange(originator: any) {
+    const { harmonics } = this.noiseParams
+    this.harmonics = Array.from(new Array(harmonics.count))
+      .map((_v, i) => {
+        // this.stats['h' + i] = { min: 1, max: 0 }
+        const period = harmonics.period / Math.pow(harmonics.spread, i)
+        const amplitude = Math.pow(harmonics.gain, i)
+        return { period, amplitude }
+      })
+    this.harmonicsAmplitudeSum = this.harmonics.reduce((sum, harm) => sum + harm.amplitude, 0)
+    this.parent?.onChange(originator)
   }
 
-  eval(input: InputType) {
+  eval(input: InputType): number {
     const { x, y } = input
     const { density } = this
-    const freq = [0.0125, 0.025, 0.05, 0.1, 0.2, 0.4, 0.8]
-    let noise = 0
-    freq.forEach((f: number, i: number) => {
-      noise += (this.noiseSource.noise3d(x * f * density, y * f * density, 0 * density) + 0.5) / Math.pow(2, i + 1)
-    })
+    let noiseEval;
+    let noise = 0;
+    this.harmonics
+      // .map(p=>1/p)    // mapping periods to frequency
+      .forEach((harmonic: Harmonic, _i: number) => {
+        noiseEval = this.noiseSource(x * density / harmonic.period, y * density / harmonic.period)
+        noise += (noiseEval * 0.5 + 0.5) * harmonic.amplitude /// Math.pow(2, i + 1)
+      })
+    noise /= this.harmonicsAmplitudeSum;
     return sanitiseNoise(noise)
   }
 }
