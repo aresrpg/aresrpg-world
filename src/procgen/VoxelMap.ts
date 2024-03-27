421e862a0fe2d6648357467174a651fc2e002931
etienne <em85@pm.me>
2024-03-27 11:58:29 +0000

    feat: on-the-fly gen with optional voxel pruning and storage handled separately

--- src/procgen/VoxelMap.ts
+++ src/procgen/VoxelMap.ts
@@ -1,15 +1,16 @@
 import { Box3, Vector3 } from 'three'
-import { OctreeIterator, PointOctree } from 'sparse-octree'
 import { IVoxelMap, IVoxelMaterial, IVoxel } from '@aresrpg/aresrpg-engine'
 
 import { VOXEL_TYPE_COLORS } from '../common/types'
 
+import { WorldGenerator } from './WorldGen'
+
 export class VoxelMap implements IVoxelMap {
   public readonly size: Vector3
-  voxelsOctree
-  public constructor(bbox: Box3) {
+  worldGen
+  public constructor(bbox: Box3, worldGen: WorldGenerator) {
     this.size = bbox.getSize(new Vector3())
-    this.voxelsOctree = new PointOctree(bbox.min, bbox.max, 0.0, 8, 8)
+    this.worldGen = worldGen
   }
 
   public readonly voxelMaterialsList = Object.values(VOXEL_TYPE_COLORS)
@@ -21,11 +22,12 @@ export class VoxelMap implements IVoxelMap {
     const bmin = new Vector3(from.x, from.y, from.z)
     const bmax = new Vector3(to.x, to.y, to.z)
     const bbox = new Box3(bmin, bmax)
-    const res = this.voxelsOctree.cull(bbox)
-    const count: number = res.reduce(
-      (count: number, node: any) => count + (node.data?.points?.length || 0),
-      0,
-    )
+    // const res = this.voxelsOctree.cull(bbox)
+    // const count: number = res.reduce(
+    //   (count: number, node: any) => count + (node.data?.points?.length || 0),
+    //   0,
+    // )
+    const count = this.worldGen.getEstimatedVoxelsCount(bbox)
     return count
   }
 
@@ -33,46 +35,11 @@ export class VoxelMap implements IVoxelMap {
     const bmin = new Vector3(from.x, from.y, from.z)
     const bmax = new Vector3(to.x - 1, to.y - 1, to.z - 1)
     const bbox = new Box3(bmin, bmax)
-    const iter = new OctreeIterator(this.voxelsOctree, bbox)
-    // console.log(`original query from/to `, from, to)
-    // console.log(`querying octree on range`, bbox)
-    function* makeGenerator() {
-      let result: any = iter.next()
-      while (!result.done) {
-        if (result.value.data) {
-          const pointOctant: any = result.value
-          const points = pointOctant.data?.points || []
-          const pointsData = pointOctant.data?.data || []
-          for (let i = 0; i < points.length; i++) {
-            const voxel: IVoxel = {
-              position: points[i],
-              materialId: pointsData[i].t,
-            }
-            // console.log("iter")
-            if (
-              voxel.position.x >= from.x &&
-              voxel.position.x < to.x &&
-              voxel.position.y >= from.y &&
-              voxel.position.y < to.y &&
-              voxel.position.z >= from.z &&
-              voxel.position.z < to.z
-            )
-              yield voxel
-            // else {
-            //   const { x, y, z } = voxel.position
-            //   console.log(`skipped voxel: x:${x}, y:${y}, z:${z} `)
-            // }
-          }
-        }
-        result = iter.next()
-      }
-    }
-    return makeGenerator()
+    return this.worldGen.generate(bbox)
   }
 
   voxelExists(x: number, y: number, z: number): boolean {
     const point = new Vector3(x, y, z)
-    const exists = !!this.voxelsOctree.findPoints(point, 0.001).length
-    return exists
+    return !!this.worldGen.getVoxel(point)
   }
 }
