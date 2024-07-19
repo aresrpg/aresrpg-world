a9162cfaf6e462f8f3f0a2493387c70129384ffc
etienne <em85@pm.me>
2024-07-19 08:36:27 +0000

    feat: patch isolation

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ export { Heightmap } from './procgen/Heightmap'
 export { NoiseSampler } from './procgen/NoiseSampler'
 export { ProcLayer } from './procgen/ProcLayer'
 export { Biome, BlockType } from './procgen/Biome'
-export { PatchCache } from './procgen/PatchCache'
-export { PatchBaseCache } from './procgen/PatchBaseCache'
-export { PatchBlocksCache } from './procgen/PatchBlocksCache'
 
 // export type { MappingConf, MappingData, MappingRanges } from "./common/types"
 // export { DevHacks } from './tools/DevHacks'
