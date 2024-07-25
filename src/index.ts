e89ad69bce6769b7fd4c89742df222509a54158b
Etienne M <em85@pm.me>
2024-07-25 06:48:58 +0000

    fix: missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
+
 export { Heightmap } from './procgen/Heightmap'
 export { NoiseSampler } from './procgen/NoiseSampler'
 export { ProcLayer } from './procgen/ProcLayer'
+export { BlocksPatch } from './procgen/BlocksPatch'
+export { ExternalCache } from './procgen/ExternalCache'
 export { Biome, BlockType } from './procgen/Biome'
+export { EntitiesMap } from './procgen/EntitiesMap'
 
 // export type { MappingConf, MappingData, MappingRanges } from "./common/types"
 // export { DevHacks } from './tools/DevHacks'
