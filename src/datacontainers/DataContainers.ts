809758880a193d7088db1e50527c8fa02a43908e
etienne <em85@pm.me>
2024-08-30 12:19:41 +0000

    refactor: end split commit

---
+++ src/datacontainers/DataContainers.ts
@@ -0,0 +1,42 @@
+/**
+ * Generic patch data container
+ */
+
+import { Vector2, Box2, Vector3 } from "three"
+import { patchLowerId, patchUpperId } from "../common/utils"
+
+// GenericPatch
+export interface GenericPatch {
+    key: any
+    bbox: any
+    chunkIds: any
+    duplicate(): GenericPatch
+    toChunks(): any
+    // toLocalPos<T = Vector2 | Vector3>(pos: T): T
+    // toGlobalPos<T = Vector2 | Vector3>(pos: T): T
+    toLocalPos(pos: Vector3): Vector3
+    toGlobalPos(pos: Vector3): Vector3
+}
+
+/**
+ * Generic PatchesMap
+ */
+export class GenericPatchesMap {
+    patchDimensions: Vector2
+    constructor(patchDim: Vector2) {
+        this.patchDimensions = patchDim
+    }
+    getPatchRange(bbox: Box2) {
+        const rangeMin = patchLowerId(bbox.min, this.patchDimensions)
+        const rangeMax = patchUpperId(bbox.max, this.patchDimensions)
+        const patchRange = new Box2(rangeMin, rangeMax)
+        return patchRange
+    }
+    getRoundedBox(bbox: Box2) {
+        const { min, max } = this.getPatchRange(bbox)
+        min.multiply(this.patchDimensions)
+        max.multiply(this.patchDimensions)
+        const extBbox = new Box2(min, max)
+        return extBbox
+    }
+}
