c4229684a92196580da362641438e00fa11c2f9d
etienne <em85@pm.me>
2024-11-28 18:08:53 +0000

    feat: reference config files

---
+++ src/demo-samples/configs/world_dev_setup.ts
@@ -0,0 +1,31 @@
+// import { SCHEMATICS_FILES_INDEX } from '../assets/schematics/index'
+import { BIOMES_LANDSCAPES_CONFIG } from './biome_landscapes'
+import { PROC_ITEMS_CONFIG } from './procedural_items'
+import { BLOCKS_COLOR_MAPPING, SCHEMATICS_BLOCKS_MAPPING } from './blocks_mappings'
+import { WorldEnv } from '../../index'
+
+/**
+ * @param worldEnv provide it to setup worker's own env or it will default to main thread env
+ */
+export const WorldEnvPresets = (worldEnv = WorldEnv.current) => {
+  worldEnv.biomes.rawConf = BIOMES_LANDSCAPES_CONFIG
+  // populate inventory with schematics and procedural objects
+  worldEnv.schematics.blocksMapping = SCHEMATICS_BLOCKS_MAPPING
+  worldEnv.proceduralItems.configs = PROC_ITEMS_CONFIG
+  // worldEnv.schematics.filesIndex = SCHEMATICS_FILES_INDEX
+  // worldEnv.chunks.dataEncoder = chunk_data_encoder
+
+  // DEBUG: uncomment following lines to enable debugging feats
+  // WorldEnv.debug.patch.borderHighlightColor = block_type.DBG_LIGHT
+  // WorldEnv.debug.schematics.missingBlockType = block_type.DBG_DARK
+}
+
+export const BlocksColorMapping = BLOCKS_COLOR_MAPPING
+
+
+/**
+ * Unified world setup to ensure having same settings everywhere (workers, main thread)
+ */
+export class WorldConfOverride extends WorldEnv {
+
+}
