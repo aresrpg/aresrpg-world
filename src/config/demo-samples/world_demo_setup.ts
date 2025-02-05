114e5b9f8c9163ab33844fbc6a9bf82e7e5aefdf
etienne <em85@pm.me>
2025-02-05 19:33:06 +0000

    fix: revert local changes

--- src/config/demo-samples/world_demo_setup.ts
+++ src/config/demo-samples/world_demo_setup.ts
@@ -1,14 +1,12 @@
 // import { SCHEMATICS_FILES_INDEX } from '../assets/schematics/index'
 import { WorldEnv } from '../../index'
-import { initWorldDevTools } from '../../tools/DevTools'
+// import { initWorldDevTools } from '../../tools/DevTools'
 import { WorldIndividualSeeds } from '../WorldEnv'
 
 import { BIOMES_LANDSCAPES_CONFIG } from './configs/biome_landscapes'
 import { PROC_ITEMS_CONFIG } from './configs/procedural_items'
 import {
   BLOCKS_COLOR_MAPPING,
-  // ExtBlock,
-  // ExtBlock,
   SCHEMATICS_BLOCKS_MAPPING,
 } from './configs/blocks_mappings'
 
@@ -21,13 +19,11 @@ const restoreOriginalSeeds = (individualSeeds: WorldIndividualSeeds) => {
 }
 
 export const setupWorldDemo = (world_env = WorldEnv.current) => {
-  world_env.seeds.main = 'test' // common seed use everywhere
-  // world_env.seaLevel = SEA_LEVEL // TODO: remove hardcoded sea
-  // world_env.chunks.dataEncoder = chunk_data_encoder
-  // world_env.chunks.dataDecoder = val => voxelmapDataPacking.getMaterialId(val)
-  // world_env.patchViewCount.near = 4 // chunks view below ground surface
+  // SEEDS
+  world_env.seeds.main = 'test' // common seed used everywhere
+  restoreOriginalSeeds(WorldEnv.current.seeds.overrides)
 
-  // EXTERNAL CONFIGS/RESOURCES
+  // EXTERNAL CONF/RESOURCES
   world_env.biomes.rawConf = BIOMES_LANDSCAPES_CONFIG
   world_env.schematics.globalBlocksMapping = SCHEMATICS_BLOCKS_MAPPING
   // world_env.schematics.filesIndex = SCHEMATICS_FILES
@@ -43,9 +39,6 @@ export const setupWorldDemo = (world_env = WorldEnv.current) => {
   // BIOME tuning
   world_env.biomes.periodicity = 8 // biome size
   world_env.biomes.bilinearInterpolationRange = 0.1
-
-  // DEV ONLY: LEAVE COMMENTED!
-  EnvOverride(world_env)
 }
 
 /**
@@ -75,7 +68,7 @@ export const EnvOverride = (worldEnv = WorldEnv.current) => {
   restoreOriginalSeeds(WorldEnv.current.seeds.overrides)
   // worldEnv.seeds.main = undefined
 
-  initWorldDevTools()
+  // initWorldDevTools()
 }
 
 export const BlocksColorOverride = (
